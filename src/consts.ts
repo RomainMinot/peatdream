28c17d87e1b3b60ad6375914e5f63ae2dde89f11
Romain Minot <rom_ino@hotmail.fr>
2025-11-02 02:02:32 +0100

    Enhance Astro configuration and structure: added UnoCSS integration, updated site title and description, removed unused font files and components, and refactored layout structure for improved organization.

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,2 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'PeatDream';
+export const SITE_DESCRIPTION = 'Dégustations de whisky et de rhum pour les amateurs!';
---
+++ uno.config.ts
@@ -0,0 +1,45 @@
+import presetWebFonts from "@unocss/preset-web-fonts";
+import presetWind4 from "@unocss/preset-wind4";
+import { DEFAULT_LAYERS, defineConfig } from "unocss";
+
+export default defineConfig({
+  presets: [
+    presetWind4(),
+    presetWebFonts({
+      provider: "google",
+      fonts: {
+        sans: "MomoTrustSans",
+      },
+    }),
+  ],
+  theme: {
+    colors: {
+      peat: {
+        50: "#fffdea",
+        100: "#fff7c5",
+        200: "#fff085",
+        300: "#ffe246",
+        DEFAULT: "#ffaf00",
+        500: "#ffaf00",
+        600: "#e28500",
+        700: "#bb5d02",
+        800: "#984708",
+        900: "#7c3b0b",
+        950: "#481d00",
+      },
+      accent: {
+        50: "#f6f7f9",
+        100: "#edeef1",
+        200: "#d6dbe1",
+        300: "#b3bcc6",
+        400: "#8997a7",
+        500: "#6b7b8c",
+        600: "#566273",
+        700: "#46505e",
+        800: "#3c4550",
+        900: "#363c44",
+        DEFAULT: "#1d2025",
+      },
+    },
+  },
+});
